aacffef45d43405bc3d92591abaccbdc3290118f
grantrudow <webdev.grant@gmail.com>
2020-09-02 13:54:18 -0500

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>USD</h1>
+            <h2>$1.255</h2>
+        </div>
+    );
+}
+
+export default Card;
