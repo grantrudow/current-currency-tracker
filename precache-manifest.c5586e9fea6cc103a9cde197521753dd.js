7cb14dbbefe4f0f72a553033a90fa65398e12b48
Grant Rudow <webdev.grant@gmail.com>
2020-09-02 20:40:15 -0500

    Updates

---
+++ precache-manifest.c5586e9fea6cc103a9cde197521753dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d949a67681a97ffe990a232ba5539d",
+    "url": "/current-currency-tracker/index.html"
+  },
+  {
+    "revision": "ab4018f016a123dc3ca2",
+    "url": "/current-currency-tracker/static/css/main.2467a53b.chunk.css"
+  },
+  {
+    "revision": "744ac3989d818860e5fa",
+    "url": "/current-currency-tracker/static/js/2.9e061ce1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/current-currency-tracker/static/js/2.9e061ce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4018f016a123dc3ca2",
+    "url": "/current-currency-tracker/static/js/main.ec1f878a.chunk.js"
+  },
+  {
+    "revision": "430db38420214351ae2b",
+    "url": "/current-currency-tracker/static/js/runtime-main.0ed4100f.js"
+  },
+  {
+    "revision": "7b116638cfeebbdaf3633fabbaafa326",
+    "url": "/current-currency-tracker/static/media/currentLogo.7b116638.png"
+  }
+]);
